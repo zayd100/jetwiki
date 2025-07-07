548a8dcd78055d9813a48a8b250e593b8041865e
zaid <zaidshabbir666@gmail.com>
2025-07-06 20:15:04 -0700

    -

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Critical for Vercel
   build: {
-    outDir: 'dist',  // Vite will put files in Front-end/dist
+    outDir: 'dist',
     emptyOutDir: true
   }
 })
